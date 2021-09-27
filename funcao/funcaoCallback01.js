08ad5dadeca0be4bf0ca735e1b71a406bb6a1fde
bschs1 <buuhzik@gmail.com>
2021-09-27 18:41:23 -0300

    callback01

--- funcao/funcaoCallback01.js
+++ funcao/funcaoCallback01.js
@@ -7,5 +7,5 @@ function imprimir(nome, indice) {
 
 fabricantes.forEach(imprimir) // for each é uma função de fabricantes, e fabricantes é um array, pra cada elemento do array ele vai chamar a função imprimir
 fabricantes.forEach(function (fabricante) { // podemos tb criar uma função pra saber so o nome dos fabricantes
-    console.log(fabricante)
-})
+    console.log(fabricante) // o evento é o loop, quando ele encontra um elemento(nesse caso fabricante) ele chama a função callback passando o elemnto em si e o indice 
+}) 
