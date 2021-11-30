bd21389af820c1059ffdcc742b09c7041701848f
bschs1 <buuhzik@gmail.com>
2021-11-30 06:37:06 -0300

    pewpew

--- node/global.js
+++ node/global.js
@@ -1,15 +1,13 @@
-
 // console.log(global)
-
 
 //como isso é global, todos outros módulos terão acesso, coisas globais normalmente são ruins pq a chance de dar caquita é grande
 global.MinhaApp = {
-    saudacao() {
-        return 'Estou em todos Lugares'
-    },
-    nome: 'Sistema Legal'
-}
+  saudacao() {
+    return 'Estou em todos Lugares';
+  },
+  nome: 'Sistema Legal',
+};
 
-function add  (a , b ) {
-    return a +       b
+function add(a, b) {
+  return a + b;
 }
--- node/globalCliente.js
+++ node/globalCliente.js
@@ -1,7 +1,7 @@
 // importando global
-require('./global') //dps de fazer isso n precisa fazer mais em nenhum arquivo pra importar
+require("./global"); //dps de fazer isso n precisa fazer mais em nenhum arquivo pra importar
 
-console.log(MinhaApp.saudacao())
+console.log(MinhaApp.saudacao());
 
-MinhaApp.nome = 'Eita!' // é mtu perigoso ter coisas globais, pq imagina um módulo besta qlqr muda uma coisa importante, monkaS
-console.log(MinhaApp.nome) // uma solução pra n acontecer isso de mudar é freezar o objeto global lá no módulo dele, global.MinhaApp = object.freeze
+MinhaApp.nome = "Eita!"; // é mtu perigoso ter coisas globais, pq imagina um módulo besta qlqr muda uma coisa importante, monkaS
+console.log(MinhaApp.nome); // uma solução pra n acontecer isso de mudar é freezar o objeto global lá no módulo dele, global.MinhaApp = object.freeze
